b07ae77c7f1ca59544c2017f6eab08157982a0bc
camelLok <8439509+camelLok@user.noreply.gitee.com>
2025-06-08 16:40:40 +0800

    update

--- examples/urtp_data_transfer_demo.c
+++ examples/urtp_data_transfer_demo.c
@@ -13,18 +13,21 @@
 #include "urtp.h"
 #include "urtp_conf.h"
 
+#define DATA_SIZE 307200
 
+static uint8_t g_data_send_buffer[DATA_SIZE] = {};
+static uint8_t g_data_recv_buffer[DATA_SIZE] = {};
 
 static int urtp_send_data(void)
 {
-
-    return 0;
+	
+	return 0;
 }
 MSH_CMD_EXPORT(urtp_send_data, Send data to the URTP receiving end.);
 
 static int urtp_recv_data(void)
 {
 
-    return 0;
+	return 0;
 }
 MSH_CMD_EXPORT(urtp_recv_data, Receive data from the sending end of the URTP.);
